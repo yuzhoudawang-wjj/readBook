cdd6a8e84efccbcb2f93f628b82cff678caefad5
wangjunjie <wangjunjie@qince.com>
2025-10-06 10:53:04 +0800

    chore: 移除首页相关文件，更新应用配置和结构

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,16 +1,11 @@
 export default defineAppConfig({
-  pages: [
-    'pages/index/index',
-    'pages/home/index',
-    'pages/tracker-list/index',
-    'pages/tracker-detail/index'
-  ],
+  pages: ['pages/home/index', 'pages/tracker-list/index', 'pages/tracker-detail/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: '追影',
     navigationBarTextStyle: 'black'
-  },
+  }
   // permission 和 requiredPrivateInfos 在新版本中已不需要
   // 如需要特殊权限，请在使用时动态申请
 })
--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { useLaunch } from '@tarojs/taro';
-import './app.less';
+import React from 'react'
+import { useLaunch } from '@tarojs/taro'
+import { TrackerProvider } from './contexts/TrackerContext'
+import { UserProvider } from './contexts/UserContext'
+import './app.less'
 
 const App: React.FC<React.PropsWithChildren> = ({ children }) => {
   useLaunch(() => {
-    console.log('App launched.');
-  });
+    console.log('App launched.')
+  })
 
   // children 是将要会渲染的页面
-  return children;
-};
+  return (
+    <UserProvider>
+      <TrackerProvider>{children}</TrackerProvider>
+    </UserProvider>
+  )
+}
 
-export default App;
+export default App
